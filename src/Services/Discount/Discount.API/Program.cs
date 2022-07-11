6691c7b96a38ef63be69b3fb759d68d77c717b8f
Raheel Javed <ch_raheel@hotmail.com>
2022-07-11 18:05:48 +0400

    Discount DB migration at startup with retry

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -24,4 +26,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MigrateDatabase<Program>();
 app.Run();
