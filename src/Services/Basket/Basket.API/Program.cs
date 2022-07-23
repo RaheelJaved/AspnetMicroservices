559f4ca44e93066d9430bea1194cc37a136c245d
Raheel Javed <ch_raheel@hotmail.com>
2022-07-23 19:43:21 +0400

    RabbitMQ/ MassTransit DI setup in Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,17 +12,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Redis Configuration
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
-
+// Grpc Configuration
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
     (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 
+// General Configuration
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddScoped<DiscountGrpcService>();
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config => { 
+    config.UsingRabbitMq((context, configurator) =>  {
+        configurator.Host(builder.Configuration.GetValue<string>("EventBusSettings:HostAddress")); //"amqp://guest:guest@localhost:5672") ;
+    });
+});
+//builder.Services.AddMassTransitHostedService();
+
 
 var app = builder.Build();
 
