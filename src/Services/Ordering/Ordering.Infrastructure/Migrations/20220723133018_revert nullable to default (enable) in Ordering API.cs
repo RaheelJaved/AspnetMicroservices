af65b0188c98e1812ff162d8a2ebe56b2ff4de2e
Raheel Javed <ch_raheel@hotmail.com>
2022-07-23 17:30:59 +0400

    revert nullable to default (enable) in Ordering API

---
+++ src/Services/Ordering/Ordering.Infrastructure/Migrations/20220723133018_revert nullable to default (enable) in Ordering API.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Migrations
+{
+    public partial class revertnullabletodefaultenableinOrderingAPI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
