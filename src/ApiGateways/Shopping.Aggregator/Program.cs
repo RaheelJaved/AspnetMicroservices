b69a65bf38f97d095ca96ed14681f98b638a706a
Raheel Javed <ch_raheel@hotmail.com>
2022-08-06 18:58:46 +0400

    Containerized shopping.aggregator gateway service

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddHttpClient<IBasketService, BasketService>(
     c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(
-    c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrll"]));
+    c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
